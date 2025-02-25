65c8f23c6ad47c89c77487f64b93e5e2d36d83e6
Dawood Faisal <dawood_faisal@yahoo.com>
2025-02-26 00:57:37 +0500

    frontend completed-4

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,39 +1,65 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import {assets} from '../../assets/assets'
-import {Link} from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
 
-const Navbar = ({setShowLogin}) => {
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
 
-    const [menu, setMenu] = useState('home')
+  const { getTotal } = useContext(StoreContext);
 
   return (
-    <div className='navbar'>
-      <Link to='/'><img src={assets.logo} alt="logo" className="logo" /></Link>
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="logo" className="logo" />
+      </Link>
 
       <ul className="navbar-menu">
-
-        <Link to='/' onClick={()=>setMenu('home')} className={menu==='home' ? 'active' : ''}>home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu('menu')} className={menu==='menu' ? 'active' : ''}>menu</a>
-        <a href='#app-download' onClick={()=>setMenu('mobile-app')} className={menu==='mobile-app' ? 'active' : ''}>mobile app</a>
-        <a href='#footer' onClick={()=>setMenu('contact-us')} className={menu==='contact-us' ? 'active' : ''}>contact us</a>
-
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
       </ul>
 
       <div className="navbar-right">
-
         <img src={assets.search_icon} alt="search icon" />
 
-        <div className='navbar-search-icon'>
-            <Link to='/cart'><img src={assets.basket_icon} alt="cart" /></Link>
-            <div className="dot"></div>
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="cart" />
+          </Link>
+          <div className={getTotal() === 0 ? "" : "dot"}></div>
         </div>
 
-        <button onClick={()=>setShowLogin(true)}>sign in</button>
-
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -20,9 +20,20 @@ const StoreContextProvider = (props) => {
     setCartItems((prev)=>({...prev, [itemId]: prev[itemId]-1}))
   }
 
-  useEffect(()=>{
-    console.log(cartItems)
-  },[cartItems])
+  const getTotal = ()=>{
+    let totalAmount = 0;
+
+    for(const item in cartItems){
+      if(cartItems[item] > 0){
+
+        let itemInfo = food_list.find((product)=>product._id === item)
+        totalAmount += itemInfo.price * cartItems[item]
+
+      }
+
+    }
+    return totalAmount
+  }
 
 
   const contextValue = {
@@ -31,6 +42,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotal,
   };
   return (
     <StoreContext.Provider value={contextValue}>
