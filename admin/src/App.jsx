8a24ca7fd35dfb7982eae300848d43d5049d802d
Dawood Faisal <dawood_faisal@yahoo.com>
2025-02-28 00:56:59 +0500

    7

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,21 +5,25 @@ import {Route, Routes} from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const url= "http://localhost:4000"
 
   return (
     <>
+    <ToastContainer/>
      <Navbar/>
      <hr />
      <div className='app-content'>
       <Sidebar />
       <Routes>
-        <Route path='/add' element={<Add/>} />
-        <Route path='/list' element={<List/>} />
-        <Route path='/list' element={<Orders/>} />
+        <Route path='/add' element={<Add url={url}/>} />
+        <Route path='/list' element={<List url={url}/>} />
+        <Route path='/list' element={<Orders url={url}/>} />
       </Routes>
      </div>
     </>
